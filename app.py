6e174c70464d446d97d91c6be1237304ee4d5c5d
JAGADEESH UNDAVALLI <84311163+UndavalliJagadeesh@users.noreply.github.com>
2022-10-31 15:12:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-
-with open('app.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-    
 import streamlit as st
 st.set_page_config(page_title="SMART WATER GRID", layout="wide")
 
